75264988d16f21fa98792e9a907563f354009799
zephyr96 <aaadev32@gmail.com>
2022-09-18 19:23:57 -0700

    added some module patterns to organize future code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
-//import './style.css';
+import './style.css';
+//create a ship factory function, be sure to set up a test
+const arraysObjectsModule = (() => {
 
-function sum(a, b) {
-    return a + b;
-}
+})();
+
+const domManipulationModule = (() => {
+
+})();
+
+const factoryFunctionModule = (() => {
+    function ship() {
+
+        carrier = {
+            length: 5,
+            health: 5,
+            sunk: false
+        }
+        battleship = {
+            length: 4,
+            health: 4,
+            sunk: false
+        }
+        destroyer = {
+            length: 3,
+            health: 3,
+            sunk: false
+        }
+        submarine = {
+            length: 3,
+            health: 3,
+            sunk: false
+        }
+        patrolBoat = {
+            length: 2,
+            health: 2,
+            sunk: false
+        }
+    }
+    return { ship };
+})();
+
 
 module.exports = { sum, };
